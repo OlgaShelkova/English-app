5334731845b8648e69a085ba72be01981f996071
OlgaShelkova <olgashelkova1@gmail.com>
2024-04-13 13:48:31 +0300

    rewrote the path to the file

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Form from "./form/Form";
-import Table from "./table/Table";
+import Form from "../form/Form";
+import Table from "../table/Table";
 
 function Content() {
   return (
